89203ebea3ed39c119a4fa1cc4bc9968e5836e67
Quangguyn <162850550+Quangguyn@users.noreply.github.com>
2024-11-24 13:55:08 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,6 +3,7 @@ let Ao = [
     id: 0,
     name: "Áo phông oversized Garfield Alway Good",
     url: "Aophong/goodfront",
+    urlback: "Aophong/goodback",
     description: `
     `,
     rating: 5,
@@ -12,6 +13,7 @@ let Ao = [
     id: 1,
     name: "Áo phông Garfield Undertall",
     url: "Aophong/undertallfront",
+    urlback: "Aophong/undertallback",
     description: `
     `,
     rating: 5,
@@ -21,6 +23,7 @@ let Ao = [
     id: 2,
     name: "Áo phông Garfield Sleep",
     url: "Aophong/sleepfront",
+    urlback: "Aophong/sleepback",
     description: `
     `,
     rating: 5,
@@ -30,6 +33,7 @@ let Ao = [
     id: 3,
     name: "Áo sơ mi nam THÊU TÚI ESSENTIAL",
     url: "Somi/smbefront",
+    urlback: "Somi/smbeback",
     description: `
     `,
     rating: 5,
@@ -39,6 +43,7 @@ let Ao = [
     id: 4,
     name: "Áo sơ mi cộc tay Regular túi ngực",
     url: "Somi/smhongfront",
+    urlback: "Somi/smhongback",
     description: `
     `,
     rating: 5,
@@ -48,6 +53,7 @@ let Ao = [
     id: 5,
     name: "Áo sơ mi dài oversized dài kẻ phối trắng",
     url: "Somi/smxanhfront",
+    urlback: "Somi/smxanhback",
     description: `
     `,
     rating: 5,
@@ -57,6 +63,7 @@ let Ao = [
     id: 6,
     name: "Áo khoác Cardigan AUTUMNAL KNIT - Moon grey",
     url: "Aokhoac/cardiganfront",
+    urlback: "Aokhoac/cardiganback",
     description: `
     `,
     rating: 5,
@@ -66,6 +73,7 @@ let Ao = [
     id: 7,
     name: "Áo khoác WOODLAND NON - COLLAR DENIM",
     url: "Aokhoac/denimfront",
+    urlback: "Aokhoac/denimback",
     description: `
     `,
     rating: 5,
@@ -75,6 +83,7 @@ let Ao = [
     id: 8,
     name: "Áo khoác TIFF DRACO LEATHER",
     url: "Aokhoac/leatherjacketfront",
+    urlback: "Aokhoac/leatherjacketback",
     description: `
     `,
     rating: 5,
